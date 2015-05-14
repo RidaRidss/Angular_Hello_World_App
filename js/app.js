7c61fd4d0181c4219552317b4c8edbf46b102d2c
RidaRidss <ridarocks12@yahoo.com>
2015-05-14 12:25:41 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
  * Created by Junaid-Pc on 5/11/2015.
  */
 var app=angular.module("MYAPP",[]);
-app.controller("helloWorldCtrl",function($scope){
+app.controller("namedCtrl",function($scope){
 $scope.name=""
 });
